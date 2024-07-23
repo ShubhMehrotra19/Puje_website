2a0c401ee98f45f44caaf420f2b36e3ba1d7a34e
Ishank Lalwani <ishanklalwani@gmail.com>
2024-07-24 00:22:06 +0530

    feat: added the contact component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,21 @@
+"use client";
+
+import dynamic from "next/dynamic";
+import React from "react";
+import "./globals.css";
+
+const GetInTouch = dynamic(() => import("@/components/contact/GetInTouch"), {
+  loading: () => <p>A map is loading</p>,
+  ssr: false,
+});
+
 export default function Home() {
   return (
-    <>
-      <div>Project Setup!</div>
-    </>
+    <div className="flex flex-col gap-20">
+      <div style={{ height: "400px", width: "100%" }}>
+        <GetInTouch posix={[19.026353, 72.851802]} />
+      </div>
+      <div style={{ fontFamily: "beaufort" }}>This is Beaufort</div>
+    </div>
   );
 }
