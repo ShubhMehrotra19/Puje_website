f11aac62bce56e93399f3f290d89540d1e283c01
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-07-23 23:05:05 +0530

    font-setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import React from 'react'
+import './globals.css'
+interface Props {}
+
+function Page(props: Props) {
+  const {} = props
+
   return (
     <>
-      <div>Project Setup!</div>
+    <div>project Setup</div>
+    <div style={{fontFamily: 'beaufort'}}>This is Beaufort</div>
     </>
-  );
+  )
 }
+
+export default Page
--- components/example/Example.tsx
+++
@@ -1,8 +0,0 @@
-//get this using the rafce snippet on VS code
-import React from "react";
-
-const Example = () => {
-  return <div>Example</div>;
-};
-
-export default Example;
