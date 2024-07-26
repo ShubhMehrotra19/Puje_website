13e34116b6282272f446615a4e68c0363d3ee27b
berobbb <shashwatdimri5@gmail.com>
2024-07-26 17:47:31 +0530

    completed combined component

--- app/page2/page.tsx
+++ app/page2/page.tsx
@@ -7,8 +7,8 @@ import ScrollTrigger from "gsap/ScrollTrigger";
 import Lenis from "@studio-freight/lenis";
 import Footer from "@/components/Footer/Footer";
 import DesignPhilosophy from "@/components/DesignPhilosophy/DesignPhilosophy";
-import Flagship from "@/components/Flagship/Flagship";
-import Ongoing from "@/components/Ongoing/Ongoing";
+import CombinedProjects from "@/components/CombinedProjects/CombinedProjects";
+
 
 const GetInTouch = dynamic(() => import("@/components/contact/GetInTouch"), {
   loading: () => <p>A map is loading</p>,
@@ -34,8 +34,7 @@ export default function Home() {
   return (
     <>
         <DesignPhilosophy />
-        <Flagship/>
-        <Ongoing/>
+       <CombinedProjects/>
         <div className="w-full">
           <GetInTouch posix={[19.026353, 72.851802]} />
         </div>
--- components/Ongoing/Ongoing.tsx
+++ components/Ongoing/Ongoing.tsx
@@ -1,59 +1,59 @@
-import React from 'react'
-import Image from "next/image";
+// import React from 'react'
+// import Image from "next/image";
 
-const Ongoing = () => {
-  return (
-    <div className="flex flex-col gap-10 mb-24">
-      <h1 className="text-3xl text-[#D7B56D] text-center">Ongoing Project</h1>
-      <hr className="border-t-2 border-[#C9C9C9] mx-10" />
-      <div className='flex justify-around'>
-        <div className="relative group flex flex-col gap-5">
-          <Image
-            src="/images/Og_Pro1.png"
-            alt="Flagship logo"
-            height={391}
-            width={423}
-            className="mx-auto transition-all duration-300 group-hover:brightness-50"
-          />
-          <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-                             bg-black text-white px-4 py-2 rounded hover:bg-black transition-all duration-300
-                             opacity-0 group-hover:opacity-100">
-            Know More
-          </button>
-          <div className='flex justify-center'>
-            <Image
-                src="/images/Og_Pro3.png"
-                alt="Flagship logo"
-                height={169}
-                width={165}
-            />
-          </div>
-        </div>
-        <div className="relative group flex flex-col gap-5">
-          <Image
-            src="/images/Og_Pro2.png"
-            alt="Flagship logo"
-            height={391}
-            width={423}
-            className="mx-auto transition-all duration-300 group-hover:brightness-50"
-          />
-          <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
-                             bg-black text-white px-4 py-2 rounded hover:bg-black transition-all duration-300
-                             opacity-0 group-hover:opacity-100">
-            Know More
-          </button>
-          <div className='flex justify-center'>
-            <Image
-                src="/images/Og_Pro4.png"
-                alt="Flagship logo"
-                height={169}
-                width={165}
-            />
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
+// const Ongoing = () => {
+//   return (
+//     <div className="flex flex-col gap-10 mb-24">
+//       <h1 className="text-3xl text-[#D7B56D] text-center">Ongoing Project</h1>
+//       <hr className="border-t-2 border-[#C9C9C9] mx-10" />
+//       <div className='flex justify-around'>
+//         <div className="relative group flex flex-col gap-5">
+//           <Image
+//             src="/images/Og_Pro1.png"
+//             alt="Flagship logo"
+//             height={391}
+//             width={423}
+//             className="mx-auto transition-all duration-300 group-hover:brightness-50"
+//           />
+//           <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+//                              bg-black text-white px-4 py-2 rounded hover:bg-black transition-all duration-300
+//                              opacity-0 group-hover:opacity-100">
+//             Know More
+//           </button>
+//           <div className='flex justify-center'>
+//             <Image
+//                 src="/images/Og_Pro3.png"
+//                 alt="Flagship logo"
+//                 height={169}
+//                 width={165}
+//             />
+//           </div>
+//         </div>
+//         <div className="relative group flex flex-col gap-5">
+//           <Image
+//             src="/images/Og_Pro2.png"
+//             alt="Flagship logo"
+//             height={391}
+//             width={423}
+//             className="mx-auto transition-all duration-300 group-hover:brightness-50"
+//           />
+//           <button className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+//                              bg-black text-white px-4 py-2 rounded hover:bg-black transition-all duration-300
+//                              opacity-0 group-hover:opacity-100">
+//             Know More
+//           </button>
+//           <div className='flex justify-center'>
+//             <Image
+//                 src="/images/Og_Pro4.png"
+//                 alt="Flagship logo"
+//                 height={169}
+//                 width={165}
+//             />
+//           </div>
+//         </div>
+//       </div>
+//     </div>
+//   )
+// }
 
-export default Ongoing
+// export default Ongoing
