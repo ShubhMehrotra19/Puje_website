7a66f314c32862508a7ba5ec289ea848c56a67d2
Stoic Coder <shubhmehrotra2022@vitbhopal.ac.in>
2024-07-29 00:53:07 +0530

    feat: animation additions

--- components/Menu/Menu.tsx
+++ components/Menu/Menu.tsx
@@ -45,6 +45,7 @@ function Menu({ className }: Props) {
         className={`relative w-screen h-screen z-20 ${className} bg-gradient-to-b from-[#7dacc0] via-[#d2d7d3] to-[#eedab7] justify-between items-center md:hidden block`}
       >
         <div className="flex flex-col pl-16 top-1/3 gap-8 justify-start items-start z-30 relative">
+        <Fade cascade direction="left" damping={0.2}>
           <p className='font-["beaufort"] pb-2 text-4xl uppercase cursor-pointer text-white hover:scale-105 hover:border-b-2 hover:border-white/60 transition-all duration-300'>
             Home
           </p>
@@ -57,6 +58,7 @@ function Menu({ className }: Props) {
           <p className='font-["beaufort"] pb-2 text-4xl uppercase cursor-pointer text-white hover:scale-105 hover:border-b-2 hover:border-white/60 transition-all duration-300'>
             Contact Us
           </p>
+          </Fade>
         </div>
         <div className="w-full h-full absolute top-0 left-0 z-10">
           <Image
